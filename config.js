59b93fc73efdd4cf78d291574a6947cbf89dd286
EQ3A1\34601 <vivinatha.84@gmail.com>
2024-10-11 11:51:36 +0200

    se hizo lo que pudo

--- config.js
+++ config.js
@@ -7,14 +7,41 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+
 });
+
+class Database {
+  constructor(id, profesor, email) {
+    this.id = id;
+    this.profesor = profesor;
+    this.email = email
+
+  }
+  nombrar(){
+    return `${this.nombre}nombre ${this.email}`
+  }
+}
+
+class Student extends Database{
+  constructor (id, nombre, email, role){
+
+  }
+} 
+
+
+
+
+
+
 
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL:", err);
     return;
-  }
+}
   console.log("Connected to MySQL!");
-});
 
-module.exports = connection;
+
+  module.exports = connection;
+})
